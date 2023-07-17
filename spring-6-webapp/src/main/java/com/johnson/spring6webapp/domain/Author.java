0ba6af53f29c33ab3df4f2e5cd9a421c413a6653
john.mukenge <mknjhn93r27z312u@sogeiesterni.it>
2023-07-17 21:39:29 +0200

    impl jpa-equals

--- spring-6-webapp/src/main/java/com/johnson/spring6webapp/domain/Author.java
+++ spring-6-webapp/src/main/java/com/johnson/spring6webapp/domain/Author.java
@@ -2,6 +2,7 @@ package com.johnson.spring6webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -62,5 +63,28 @@ public class Author {
         this.lastName = lastName;
     }
 
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
--- spring-6-webapp/src/main/java/com/johnson/spring6webapp/domain/Book.java
+++ spring-6-webapp/src/main/java/com/johnson/spring6webapp/domain/Book.java
@@ -2,6 +2,7 @@ package com.johnson.spring6webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -64,5 +65,28 @@ public class Book {
         this.isbn = isbn;
     }
 
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
