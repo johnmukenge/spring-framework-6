79384cf8b4f8ae9c27c8d248c736db9d43dcc338
john.mukenge <mknjhn93r27z312u@sogeiesterni.it>
2023-07-17 21:32:21 +0200

    - initial commit - jpa-entities - jpa-relationship

---
+++ spring-6-webapp/src/test/java/com/johnson/spring6webapp/Spring6WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnson.spring6webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
